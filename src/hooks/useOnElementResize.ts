ab55f613a6c6c18439428e437bb98cab35e312a4
CarusoX <carusox@protonmail.com>
2022-02-25 10:54:24 -0300

    package setup

---
+++ src/hooks/useOnElementResize.ts
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useRef, useMemo } from 'react';
+
+// Custom hook to track dimension of a component
+// Taken and modified from https://stackoverflow.com/a/60218754
+export const useOnElementResize = <T extends HTMLElement>(
+  targetRef: React.RefObject<T>,
+  callback: ResizeObserverCallback,
+): void => {
+  const observer = useRef<ResizeObserver>();
+
+  useEffect(() => {
+    observer.current = new ResizeObserver(callback);
+    observer.current.observe(targetRef.current as Element);
+
+    return () => {
+      observer.current?.disconnect();
+    };
+  }, [targetRef]);
+};
---
+++ src/hooks/useSoftHyphenation.ts
@@ -0,0 +1,71 @@
+import React, { useLayoutEffect } from 'react';
+import { useOnElementResize } from './useOnElementResize';
+
+const getElementHeight = <T extends HTMLElement>(ref: React.RefObject<T>) => {
+  if (!ref.current) return 0;
+  const { height } = ref.current.getBoundingClientRect();
+  return height;
+};
+
+// Check we don't overflow container
+const checkStringFitsOnBox = <T extends HTMLElement>(ref: React.RefObject<T>, str: string) => {
+  if (!ref.current) return false;
+  const backup = ref.current.innerText;
+  ref.current.innerText = str;
+  const overflows = ref.current.scrollWidth > ref.current.clientWidth;
+  ref.current.innerText = backup;
+  return !overflows;
+};
+
+// Check we don't overflow container + we don't have an extra line
+const checkStringFitsOnLine = <T extends HTMLElement>(ref: React.RefObject<T>, str: string) => {
+  if (!ref.current) return false;
+  const backup = ref.current.innerText;
+  const currentHeight = getElementHeight(ref);
+  ref.current.innerText = str;
+  const nextHeight = getElementHeight(ref);
+  ref.current.innerText = backup;
+  return currentHeight === nextHeight && checkStringFitsOnBox(ref, str);
+};
+
+const hyphenate = <T extends HTMLElement>(ref: React.RefObject<T>, str: string) => {
+  const element = ref.current;
+  if (!element) return;
+
+  const chars = str.split('');
+
+  let newHyphenatedContent = '';
+
+  for (let index = 0; index < chars.length; index = index + 0) {
+    element.innerText = newHyphenatedContent;
+    const char = chars[index];
+    // We try to add the character with a hyphen before it
+    const fits = checkStringFitsOnLine(ref, `${newHyphenatedContent}-${char}`);
+    if (index === 0 || fits) {
+      // If first character, or character fits with hyphen
+      newHyphenatedContent += char;
+    } else if (char.match(/\s/)) {
+      // If we have a space character, we don't need to do anything
+      newHyphenatedContent += char;
+    } else {
+      const fitsOnBox = checkStringFitsOnBox(ref, `${newHyphenatedContent}${char}`);
+      const fitsOnBoxWithHyphen = checkStringFitsOnBox(ref, `${newHyphenatedContent}${char}-`);
+      if (fitsOnBox && fitsOnBoxWithHyphen) {
+        newHyphenatedContent += char;
+      } else {
+        // Add soft hyphen since we don't want it to be copy-pastable
+        newHyphenatedContent += `\u00AD${char}`;
+      }
+    }
+    index = index + 1;
+  }
+
+  element.innerText = newHyphenatedContent;
+};
+
+export const useSoftHyphenation = <T extends HTMLElement>(ref: React.RefObject<T>, innerText: string) => {
+  useLayoutEffect(() => {
+    hyphenate(ref, innerText);
+  });
+  useOnElementResize(ref, () => hyphenate(ref, innerText));
+};
